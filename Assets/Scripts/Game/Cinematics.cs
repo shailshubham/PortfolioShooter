f4950bc422a6de0458cbc892cd647b10e51dbd44
shailshubham <56213963+shailshubham@users.noreply.github.com>
2023-05-01 22:48:07 +0530

    working on gameplay cinematic done

--- Assets/Scripts/Game/Cinematics.cs
+++ Assets/Scripts/Game/Cinematics.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class GameManager : MonoBehaviour
+public class Cinematics : MonoBehaviour
 {
-    public GameObject player;
+    PlayableDirector director;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        director = GetComponent<PlayableDirector>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
     {
         
     }
